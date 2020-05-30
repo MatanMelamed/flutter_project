e78e09fefd5af398a25a354fbb8ffebed6cb4504
Matan Melamed <melamed.matan1@gmail.com>
2020-05-30 17:53:39 +0300

    auto login for tests created

--- lib/Tests/auto_login.dart
+++ lib/Tests/auto_login.dart
@@ -1,25 +1,19 @@
-import 'dart:html';
-
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:teamapp/models/user.dart';
+import 'package:teamapp/screens/home/home.dart';
 import 'package:teamapp/services/authenticate/auth_service.dart';
 import 'package:teamapp/services/firestore/userDataManager.dart';
+import 'package:teamapp/widgets/loading.dart';
 
-enum DummyUsers{
-  Mike,
-  Motek,
-  Elizabeth,
-  Michael
-}
+enum DummyUsers { Mike, Motek, Elizabeth, Michael }
 
 class AutoLogin extends StatelessWidget {
+  final DummyUsers user;
+  final Widget child;
+  final Map<DummyUsers, String> userToUid = new Map();
 
-  DummyUsers user;
-  Map<DummyUsers,String> userToUid;
-
-  AutoLogin(this.user){
-    userToUid = new Map();
+  AutoLogin({@required this.user, this.child}) {
     userToUid[DummyUsers.Mike] = '6XmkIWBQOQTSH1DKNkqpNl697mF3';
     userToUid[DummyUsers.Motek] = '3vgCItKZeXNQemeewn59rfrZFfV2';
     userToUid[DummyUsers.Elizabeth] = 'C5h3rKCR9Rh7qbGmfc3didEuZlu1';
@@ -28,23 +22,17 @@ class AutoLogin extends StatelessWidget {
 
   login() async {
     AuthService authService = AuthService();
-    String email = (await UserDataManager.getUser(userToUid[user])).e;
-    authService.signInWithEmailAndPassword(email, password)
+    String email = (await UserDataManager.getUser(userToUid[user])).email;
+    authService.signInWithEmailAndPassword(email, '123123123');
   }
 
   @override
   Widget build(BuildContext context) {
     final user = Provider.of<User>(context);
-    if(user == null){
+    if (user == null) {
       login();
     }
 
-    return Container(
-
-
-    );
+    return user == null ? Loading() : child ?? Home();
   }
-
-
 }
-
--- lib/main.dart
+++ lib/main.dart
@@ -1,27 +1,22 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:teamapp/Tests/auto_login.dart';
 import 'package:teamapp/Tests/creators/viewTeamPageTester.dart';
 import 'package:teamapp/screens/teams/team_page.dart';
 import 'package:teamapp/services/authenticate/auth_service.dart';
-import 'package:teamapp/widgets/auth_warrper.dart';
-import 'Tests/creators/viewUsersListTester.dart';
+import 'package:teamapp/services/firestore/teamDataManager.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-
-
   @override
   Widget build(BuildContext context) {
     AuthService authService = AuthService();
     return StreamProvider.value(
       value: authService.userStream, // what the provider listens to
       child: MaterialApp(
-        //home: AuthWrapper(),
-//        home: ViewTeamPageTester(),
-//        home: Test_CreateUsersListPage(),
-        home: AuthWrapper(),
-        ),
-      );
+        home: AutoLogin(user: DummyUsers.Michael,child: ViewTeamPageTester()),
+      ),
+    );
   }
 }
